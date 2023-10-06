716ec223717d5bc7a435a1e70b9e01c1a5ed4c6e
Abhishek Kakkar <kakkarabhee@gmail.com>
2023-10-06 19:04:01 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express")
+const dotenv = require("dotenv")
+dotenv.config()
+const cors = require("cors")
+const path=require("path")
+
+const router = require("./Routes/index")
+
+require("./dbConnect")
+const app = express()
+app.use(cors())
+
+app.use("/public", express.static("public"))
+app.use(express.static(path.join(__dirname,'build')))
+
+app.use(express.json())
+app.use("/api", router)
+
+app.use('*', express.static(path.join(__dirname,'build')))
+
+const PORT = process.env.PORT || 8000
+app.listen(PORT, () => console.log(`Server is Running at port no ${PORT}`))
